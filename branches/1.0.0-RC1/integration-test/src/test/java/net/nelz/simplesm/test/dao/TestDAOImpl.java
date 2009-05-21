529761bd376be861eb26eda4e89f1c8e24c13bbb
nelz9999 <nelz9999@565fef7e-9d32-11dd-b397-3d94ef37118f>
2009-05-21 03:04:32 +0000

    Cutting the first release candidate for version 1.0.0

---
+++ branches/1.0.0-RC1/integration-test/src/test/java/net/nelz/simplesm/test/dao/TestDAOImpl.java
@@ -0,0 +1,148 @@
+package net.nelz.simplesm.test.dao;
+
+import net.nelz.simplesm.annotations.*;
+import org.springframework.stereotype.*;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.math.RandomUtils;
+
+import java.util.*;
+
+/**
+Copyright (c) 2008, 2009  Nelson Carpentier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+@Repository("testDao")
+public class TestDAOImpl implements TestDAO {
+
+	@ReadThroughSingleCache(namespace = "Alpha", keyIndex = 0, expiration = 30)
+	public String getDateString(final String key) {
+		final Date now = new Date();
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException ex) {}
+		return now.toString() + ":" + now.getTime();
+	}
+
+    @UpdateSingleCache(namespace = "Alpha", keyIndex = 1, dataIndex = 2, expiration = 30)
+    public void overrideDateString(final int trash, final String key, final String overrideData) {}
+
+    @ReadThroughMultiCache(namespace = "Bravo", keyIndex = 0, expiration = 300)
+	public List<String> getTimestampValues(final List<Long> keys) {
+		final List<String> results = new ArrayList<String>();
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException ex) {}
+		final Long now = new Date().getTime();
+		for (final Long key : keys) {
+			results.add(now.toString() + "-X-" + key.toString());
+		}
+		return results;
+	}
+
+	@UpdateSingleCache(namespace = "Bravo", keyIndex = 0, expiration = 300, dataIndex = -1)
+	public String updateTimestampValue(final Long key) {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException ex) {}
+		final Long now = new Date().getTime();
+		final String result = now.toString() + "-U-" + key.toString();
+		return result;
+	}
+
+	@UpdateMultiCache(namespace = "Bravo", keyIndex = 0, expiration = 300, dataIndex = -1)
+	public List<String> updateTimestamValues(final List<Long> keys) {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException ex) {}
+		final Long now = new Date().getTime();
+		final List<String> results = new ArrayList<String>();
+		for (final Long key : keys) {
+			results.add(now.toString() + "-M-" + key.toString());
+		}
+		return results;
+	}
+
+    @UpdateMultiCache(namespace = "Bravo", keyIndex = 1, dataIndex = 3, expiration = 300)
+    public void overrideTimestampValues(final int trash,
+                                        final List<Long> keys,
+                                        final String nuthin,
+                                        final List<String> overrideData) {}
+
+    @ReadThroughSingleCache(namespace = "Charlie", keyIndex = 0, expiration = 1000)
+    public String getRandomString(final Long key) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {}
+        return RandomStringUtils.randomAlphanumeric(25 + RandomUtils.nextInt(30));
+    }
+
+    @InvalidateSingleCache(namespace = "Charlie", keyIndex = 0)
+    public void updateRandomString(final Long key) {
+        // Nothing really to do here.
+    }
+
+    @InvalidateSingleCache(namespace = "Charlie", keyIndex = -1)
+    public Long updateRandomStringAgain(final Long key) {
+        return key;
+    }
+
+    @ReadThroughMultiCache(namespace = "Delta", keyIndex = 0, expiration = 1000)
+    public List<String> getRandomStrings(final List<Long> keys) {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {}
+        final String series = RandomStringUtils.randomAlphabetic(6);
+        final List<String> results = new ArrayList<String>(keys.size());
+        for (final Long key : keys) {
+            results.add(series + "-" + key.toString() + "-" + RandomStringUtils.randomAlphanumeric(25 + RandomUtils.nextInt(30)));
+        }
+        return results;
+    }
+
+    @InvalidateMultiCache(namespace = "Delta", keyIndex = 0)
+    public void updateRandomStrings(final List<Long> keys) {
+        // Nothing to do.
+    }
+
+    @InvalidateMultiCache(namespace = "Delta", keyIndex = 0)
+    public List<Long> updateRandomStringsAgain(final List<Long> keys) {
+        return keys;
+    }
+
+    @ReadThroughAssignCache(assignedKey = "SomePhatKey", namespace = "Echo", expiration = 3000)
+    public List<String> getAssignStrings() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {}
+        final List<String> results = new ArrayList<String>();
+        final long extra = System.currentTimeMillis() % 20;
+        final String base = System.currentTimeMillis() + "";
+        for (int ix = 0; ix < 20 + extra; ix++) {
+            results.add(ix + "-" + base);
+        }
+        return results;
+    }
+
+    @InvalidateAssignCache(assignedKey = "SomePhatKey", namespace = "Echo")
+    public void invalidateAssignStrings() { }
+
+    @UpdateAssignCache(assignedKey = "SomePhatKey", namespace = "Echo", expiration = 3000, dataIndex = 1)
+    public void updateAssignStrings(int bubpkus, final List<String> newData) { }
+}
---
+++ branches/1.0.0-RC1/integration-test/src/test/java/net/nelz/simplesm/test/svc/TestSvcImpl.java
@@ -0,0 +1,99 @@
+package net.nelz.simplesm.test.svc;
+
+import net.nelz.simplesm.test.dao.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+/**
+Copyright (c) 2008, 2009  Nelson Carpentier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+@Service("testSvc")
+public class TestSvcImpl implements TestSvc {
+
+	private TestDAO dao;
+
+	public void setDao(TestDAO dao) {
+		this.dao = dao;
+	}
+
+	public String getDateString(final String key) {
+		return this.dao.getDateString(key);
+	}
+
+    public void overrideDateString(final int trash, final String key, final String overrideData) {
+        dao.overrideDateString(trash, key, overrideData);
+    }
+
+	public List<String> getTimestampValues(final List<Long> keys) {
+		return this.dao.getTimestampValues(keys);
+	}
+
+	public String updateTimestampValue(final Long key) {
+		return this.dao.updateTimestampValue(key);
+	}
+
+	public List<String> updateTimestamValues(final List<Long> keys) {
+		return this.dao.updateTimestamValues(keys);
+	}
+
+    public void overrideTimestampValues(final int trash, final List<Long> keys,
+                                        final String nuthin, final List<String> overrideData) {
+        dao.overrideTimestampValues(trash, keys, nuthin, overrideData);
+    }
+
+    public String getRandomString(final Long key) {
+        return this.dao.getRandomString(key);
+    }
+
+    public void updateRandomString(final Long key) {
+        this.dao.updateRandomString(key);
+    }
+
+    public Long updateRandomStringAgain(final Long key) {
+        return this.dao.updateRandomStringAgain(key);
+    }
+
+    public List<String> getRandomStrings(List<Long> keys) {
+        return this.dao.getRandomStrings(keys);
+    }
+
+    public void updateRandomStrings(List<Long> keys) {
+        this.dao.updateRandomStrings(keys);
+    }
+
+    public List<Long> updateRandomStringsAgain(List<Long> keys) {
+        return this.dao.updateRandomStringsAgain(keys);
+    }
+
+    public List<String> getAssignStrings() {
+        return this.dao.getAssignStrings();
+    }
+
+    public void invalidateAssignStrings() {
+        this.dao.invalidateAssignStrings();
+    }
+
+    public void updateAssignStrings(final List<String> newData) {
+        this.dao.updateAssignStrings(25, newData);
+    }
+
+}
